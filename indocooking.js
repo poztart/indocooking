ed27f12cf69601cbc1f94ec3a47dbf5a5db8d764
poztart <poztart@gmail.com>
2019-01-30 12:08:24 +0700

    Add files via upload

---
+++ indocooking.js
@@ -0,0 +1,36 @@
+var b_txt = '';
+
+// write the badge
+	
+	
+		 	 		
+	b_txt+= '<div class="flickr_badge_image" id="cooking.id"><a href="https://moderncookingcuisine.blogspot.com/2017/02/resep-lezat-pastel-isi-rebung-pedas.html"><img src="https://2.bp.blogspot.com/-rl8vA0deM54/WKrQLK2OjgI/AAAAAAAAA7c/jIxWIVUyp6U8Ga579541UhHE_uyAat9FwCLcB/w260-h170-c/pastel_isi.png" alt="Pastel Isi Rebung Pedas" title="Pastel Isi Rebung Pedas" height="75" width="75"></a></div>';
+	
+		 	 		
+	b_txt+= '<div class="flickr_badge_image" id="cooking.id"><a href="https://moderncookingcuisine.blogspot.com/2017/02/chicken-cooked-with-coconut-milk-opor.html"><img src="https://1.bp.blogspot.com/-D1-ZoJSfxPM/WKX3f8Un0jI/AAAAAAAAA60/YHiLBQ0GXDY9bVte7nTMNhjFPD7bQWybQCLcB/w260-h170-c/opor_ayam.png" alt="Opor Ayam Kampung" title="Opor Ayam Kampung" height="75" width="75"></a></div>';
+	
+		 	 		
+	b_txt+= '<div class="flickr_badge_image" id="cooking.id"><a href="https://moderncookingcuisine.blogspot.com/2017/02/resep-rendang-jengkol-nikmat-istimewa.html"><img src="https://1.bp.blogspot.com/-v9ZhRlflerY/WKWGx7Tm3pI/AAAAAAAAA6E/0di-ChUHmpgEVd1pX2KpuKyb7LLouH58gCLcB/w260-h170-c/rendang%2Bjengkol.png" alt="Rendang Jengkol" title="Rendang Jengkol Istimewa" height="75" width="75"></a></div>';
+	
+		 	 		
+	b_txt+= '<div class="flickr_badge_image" id="cooking.id"><a href="https://moderncookingcuisine.blogspot.com/2017/02/resep-lezat-chicken-fire-teriyaki.html"><img src="https://1.bp.blogspot.com/-rotZhypVc1I/WKHe-lfpfSI/AAAAAAAAA5M/9w4VV5MRm28m6nhW_3n3FuUdYOVbZReOgCLcB/w260-h170-c/chicken%2Bfire%2Bteriyaki.png" alt="Best Indonesian Recipe" title="Chicken Fire Teriyaki" height="75" width="75"></a></div>';
+	
+		 	 		
+	b_txt+= '<div class="flickr_badge_image" id="cooking.id"><a href="https://moderncookingcuisine.blogspot.com/2017/02/resep-membuat-puding-lumut-istimewa.html"><img src="https://2.bp.blogspot.com/-4OOSVd9QLDI/WKCNtNZRw9I/AAAAAAAAA3k/DIbWn5qLjBYcCPwZ6tt0h7NG4j4HD0xPACLcB/w260-h170-c/puding%2Blumut.jpg" alt="Pudding Lumut Istimewa" title="Pudding Lumut Istimewa" height="75" width="75"></a></div>';
+	
+		 	 		
+	b_txt+= '<div class="flickr_badge_image" id="cooking.id"><a href="https://moderncookingcuisine.blogspot.com/2017/02/cara-membuat-wedang-ronde-karakter.html"><img src="https://3.bp.blogspot.com/-rz6l8qhsefY/WKriURicrGI/AAAAAAAAA7s/9Ppa2EOIR9wtkdLngbQFuDUUy27P4t1MgCLcB/s640/wedang%2Bronde%2Bkarakter.png" alt="Cara Membuat Wedang Ronde" title="Wedang Ronde Karakter" height="75" width="75"></a></div>';
+	
+		 	 		
+	b_txt+= '<div class="flickr_badge_image" id="cooking.id"><a href="https://moderncookingcuisine.blogspot.com/2017/02/resep-cara-membuat-nugget-tahu-wortel.html"><img src="https://4.bp.blogspot.com/-OqbDQJJ36rs/WKIIMR0RHEI/AAAAAAAAA5o/IZrJSjWNUpo4MhLkfevY2t8z1d8PNBcywCLcB/s72-c/nugget_tahu.png" alt="Nugget Tahu" title="Nugget Tahu Wortel" height="75" width="75"></a></div>';
+	
+		 	 		
+	b_txt+= '<div class="flickr_badge_image" id="cooking.id"><a href="https://moderncookingcuisine.blogspot.com/2017/02/tentang-cooking-and-cuisine-resep.html"><img src="https://4.bp.blogspot.com/-rkBJ_9hFXtc/WJ4ckuyRqEI/AAAAAAAAA2A/t6-h6NSd11sJZOvaczTKikviCKlLpdfmQCLcB/s72-c/bakmi%2Bgoreng.jpg" alt="Indonesian Cooking" title="Best Indonesian Cooking" height="75" width="75"></a></div>';
+	
+		 	 		
+	b_txt+= '<div class="flickr_badge_image" id="cooking.id"><a href="https://moderncookingcuisine.blogspot.com"><img src="https://1.bp.blogspot.com/-AozVthC8tSg/WKDXFBPW8gI/AAAAAAAAA4o/0lCSW4LASts7pnRh7Jc58G5zE_GhPAfeACK4B/w457-h306-p/cooking2.png" alt="Best Indonesian Cooking" title="Indonesian Cooking Recipe" height="75" width="75"></a></div>';
+
+
+b_txt += '<span style="position:absolute;left:-999em;top:-999em;visibility:hidden" class="flickr_badge_beacon"><img src="https://geo.yahoo.com/p?s=792600102&t=cb35e38cb69ec4693c59fac08d8983e0&r=http%3A%2F%2Fwww.cooking.id%2Fsearch%2Flabel%2FCamilan&fl_ev=0&lang=en&intl=id" width="0" height="0" alt="" /></span>';
+
+document.write(b_txt);
